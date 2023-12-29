866e2332e04b4aa1ba5a059cc8b3b1887a86a120
Genju <derfloh205@gmail.com>
2023-12-29 20:25:28 +0100

    Library Versioning Update

--- Libs/Libs.lua
+++ Libs/Libs.lua
@@ -1,8 +1,8 @@
 ---@class PvPLookup
 local PvPLookup = select(2, ...)
 
----@type GGUI
-PvPLookup.GGUI = LibStub:GetLibrary("GGUI")
+---@type GGUI-2.0
+PvPLookup.GGUI = LibStub:GetLibrary("GGUI-2.0")
 
----@type GUTIL
-PvPLookup.GUTIL = LibStub:GetLibrary("GUTIL")
+---@type GUTIL-2.0
+PvPLookup.GUTIL = LibStub:GetLibrary("GUTIL-2.0")
