4a22f30dd71de644b45e14d457de66c9a1ad2a01
Ulminia <ulminia@gmail.com>
2024-03-06 04:26:30 -0400

    lib updates

--- Libs/Libs.lua
+++ Libs/Libs.lua
@@ -2,7 +2,7 @@
 local Arenalogs = select(2, ...)
 
 ---@type GGUI-2.1
-Arenalogs.GGUI = LibStub:GetLibrary("GGUI-2.1")
+Arenalogs.GGUI = LibStub:GetLibrary("GGUI")
 
 ---@type GUTIL-2.0
-Arenalogs.GUTIL = LibStub:GetLibrary("GUTIL-2.0")
+Arenalogs.GUTIL = LibStub:GetLibrary("GUTIL")
