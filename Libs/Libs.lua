befdced70ac7fed858dcdbfa29d5f62fca3a54e5
Eyal Alon <3756641+iam3yal@users.noreply.github.com>
2024-04-02 16:34:36 +0300

    Update .gitignore
    Libs.lua and classic.lua shouldn't be ignored.

---
+++ Libs/Libs.lua
@@ -0,0 +1,8 @@
+---@class PvPAssistant
+local PvPAssistant = select(2, ...)
+
+---@type GGUI-2.1
+PvPAssistant.GGUI = LibStub:GetLibrary("GGUI-2.1")
+
+---@type GUTIL-2.0
+PvPAssistant.GUTIL = LibStub:GetLibrary("GUTIL-2.0")
