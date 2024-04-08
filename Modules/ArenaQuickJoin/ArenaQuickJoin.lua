338806ca409e5bbee07ecba3d2a14a36895fb7fb
Genju <derfloh205@gmail.com>
2024-04-08 13:10:45 +0200

    Temp Disable ArenaQuickJoin

--- Modules/ArenaQuickJoin/ArenaQuickJoin.lua
+++ Modules/ArenaQuickJoin/ArenaQuickJoin.lua
@@ -81,146 +81,146 @@ local function ShowTooltipStateInfo(selectedBracketButton)
 end
 
 local joinMacroButton, configureMacroButton, selectedBracketButton
-frame:SetScript("OnEvent", function(_, eventName, ...)
-    if eventName == "PLAYER_LOGIN" then
-        do
-            local parent = PvPAssistant.MAIN_FRAME:GetParentFrame()
+-- frame:SetScript("OnEvent", function(_, eventName, ...)
+--     if eventName == "PLAYER_LOGIN" then
+--         do
+--             local parent = PvPAssistant.MAIN_FRAME:GetParentFrame()
 
-            joinMacroButton = CreateFrame("Button", "PvPAssistant_ArenaQuickJoinMacroButton", parent,
-                "SecureActionButtonTemplate, SecureHandlerStateTemplate, ActionButtonTemplate")
+--             joinMacroButton = CreateFrame("Button", "PvPAssistant_ArenaQuickJoinMacroButton", parent,
+--                 "SecureActionButtonTemplate, SecureHandlerStateTemplate, ActionButtonTemplate")
 
-            function joinMacroButton:Active(style)
-                if style == "show" then
-                    self:SetAlpha(1)
-                elseif style == "normal" then
-                    -- NOTE: Can't be called during combat
-                    self:Enable()
-                    self.icon:SetDesaturated(false)
-                end
-            end
+--             function joinMacroButton:Active(style)
+--                 if style == "show" then
+--                     self:SetAlpha(1)
+--                 elseif style == "normal" then
+--                     -- NOTE: Can't be called during combat
+--                     self:Enable()
+--                     self.icon:SetDesaturated(false)
+--                 end
+--             end
 
-            function joinMacroButton:Inactive(style)
-                if style == "hide" then
-                    self:SetAlpha(0)
-                elseif style == "grayout" then
-                    -- NOTE: Can't be called during combat
-                    self:Disable()
-                    self.icon:SetDesaturated(true)
-                end
-            end
+--             function joinMacroButton:Inactive(style)
+--                 if style == "hide" then
+--                     self:SetAlpha(0)
+--                 elseif style == "grayout" then
+--                     -- NOTE: Can't be called during combat
+--                     self:Disable()
+--                     self.icon:SetDesaturated(true)
+--                 end
+--             end
 
-            function joinMacroButton:SetTexture(texture)
-                self.icon:SetTexture("Interface\\Icons\\" .. texture)
-            end
+--             function joinMacroButton:SetTexture(texture)
+--                 self.icon:SetTexture("Interface\\Icons\\" .. texture)
+--             end
 
-            joinMacroButton:SetPoint("TOP", parent, "TOP", 275, -35)
-            joinMacroButton:SetSize(45, 45)
-            joinMacroButton:SetScale(.6)
-            joinMacroButton:RegisterForClicks('AnyUp', 'AnyDown')
-            joinMacroButton:SetTexture("achievement_bg_killxenemies_generalsroom")
-            joinMacroButton:SetAttribute("type", "macro")
-            joinMacroButton:Inactive("grayout")
-        end
+--             joinMacroButton:SetPoint("TOP", parent, "TOP", 275, -35)
+--             joinMacroButton:SetSize(45, 45)
+--             joinMacroButton:SetScale(.6)
+--             joinMacroButton:RegisterForClicks('AnyUp', 'AnyDown')
+--             joinMacroButton:SetTexture("achievement_bg_killxenemies_generalsroom")
+--             joinMacroButton:SetAttribute("type", "macro")
+--             joinMacroButton:Inactive("grayout")
+--         end
 
-        local _, isLoaded = IsAddOnLoaded(PVPUI_ADDON_NAME)
+--         local _, isLoaded = IsAddOnLoaded(PVPUI_ADDON_NAME)
 
-        if not isLoaded then
-            UIParentLoadAddOn(PVPUI_ADDON_NAME)
-        end
+--         if not isLoaded then
+--             UIParentLoadAddOn(PVPUI_ADDON_NAME)
+--         end
 
-        do
-            local tooltipHandle
+--         do
+--             local tooltipHandle
 
-            local tooltip = function()
-                ShowTooltipStateInfo(selectedBracketButton)
-            end
+--             local tooltip = function()
+--                 ShowTooltipStateInfo(selectedBracketButton)
+--             end
 
-            local showAndUpdateTooltip = function()
-                tooltipHandle = NewTicker(0, tooltip)
-            end
+--             local showAndUpdateTooltip = function()
+--                 tooltipHandle = NewTicker(0, tooltip)
+--             end
 
-            local hideTooltip = function()
-                if tooltipHandle then
-                    tooltipHandle:Cancel()
-                end
-                GameTooltip:Hide()
-            end
+--             local hideTooltip = function()
+--                 if tooltipHandle then
+--                     tooltipHandle:Cancel()
+--                 end
+--                 GameTooltip:Hide()
+--             end
 
-            joinMacroButton:SetScript("OnEnter", function(self)
-                GameTooltip:SetOwner(self, "ANCHOR_TOPLEFT", 0, 0)
-                showAndUpdateTooltip()
-            end)
+--             joinMacroButton:SetScript("OnEnter", function(self)
+--                 GameTooltip:SetOwner(self, "ANCHOR_TOPLEFT", 0, 0)
+--                 showAndUpdateTooltip()
+--             end)
 
-            joinMacroButton:SetScript("OnLeave", hideTooltip)
-        end
-    elseif eventName == "ADDON_LOADED" then
-        local arg1 = ...
+--             joinMacroButton:SetScript("OnLeave", hideTooltip)
+--         end
+--     elseif eventName == "ADDON_LOADED" then
+--         local arg1 = ...
 
-        if arg1 ~= PVPUI_ADDON_NAME then
-            return
-        end
+--         if arg1 ~= PVPUI_ADDON_NAME then
+--             return
+--         end
 
-        configureMacroButton = function(self)
-            self:Active("normal")
+--         configureMacroButton = function(self)
+--             self:Active("normal")
 
-            frame:RegisterEvent("GROUP_ROSTER_UPDATE")
-            frame:RegisterEvent("MODIFIER_STATE_CHANGED")
+--             frame:RegisterEvent("GROUP_ROSTER_UPDATE")
+--             frame:RegisterEvent("MODIFIER_STATE_CHANGED")
 
-            self:SetFrameRef("PVEFrame", PVEFrame)
-            self:SetFrameRef("GroupSizeButton", GetGroupSizeButton())
-            self:SetFrameRef("ConquestJoinButton", ConquestJoinButton)
+--             self:SetFrameRef("PVEFrame", PVEFrame)
+--             self:SetFrameRef("GroupSizeButton", GetGroupSizeButton())
+--             self:SetFrameRef("ConquestJoinButton", ConquestJoinButton)
 
-            do
-                local NO_OP_BUTTON = CreateFrame("Button", nil, nil, "SecureActionButtonTemplate")
-                hooksecurefunc("ConquestFrame_SelectButton", function(frameSelectedButton)
-                    if InCombat() then return end
-                    if ConquestJoinButton:IsEnabled() then
-                        selectedBracketButton = frameSelectedButton
-                        self:SetFrameRef("SelectedButton", frameSelectedButton)
-                    else
-                        selectedBracketButton = NO_OP_BUTTON
-                        self:SetFrameRef("SelectedButton", NO_OP_BUTTON)
-                    end
-                end)
-            end
+--             do
+--                 local NO_OP_BUTTON = CreateFrame("Button", nil, nil, "SecureActionButtonTemplate")
+--                 hooksecurefunc("ConquestFrame_SelectButton", function(frameSelectedButton)
+--                     if InCombat() then return end
+--                     if ConquestJoinButton:IsEnabled() then
+--                         selectedBracketButton = frameSelectedButton
+--                         self:SetFrameRef("SelectedButton", frameSelectedButton)
+--                     else
+--                         selectedBracketButton = NO_OP_BUTTON
+--                         self:SetFrameRef("SelectedButton", NO_OP_BUTTON)
+--                     end
+--                 end)
+--             end
 
-            SecureHandlerWrapScript(self, "OnClick", self, [[
-                local PVEFrame = self:GetFrameRef("PVEFrame")
-                local SelectedButton = self:GetFrameRef("SelectedButton")
-                local GroupSizeButton = self:GetFrameRef("GroupSizeButton")
+--             SecureHandlerWrapScript(self, "OnClick", self, [[
+--                 local PVEFrame = self:GetFrameRef("PVEFrame")
+--                 local SelectedButton = self:GetFrameRef("SelectedButton")
+--                 local GroupSizeButton = self:GetFrameRef("GroupSizeButton")
 
-                if PVEFrame:IsVisible() then
-                    self:SetAttribute("macrotext", "/click LFDMicroButton")
-                elseif IsAltKeyDown() then
-                    self:SetAttribute("macrotext", "/click LFDMicroButton\n/click PVEFrameTab2\n/click PVPQueueFrameCategoryButton1")
-                elseif GroupSizeButton ~= SelectedButton or IsControlKeyDown() then
-                    self:SetAttribute("macrotext", "/click LFDMicroButton\n/click PVEFrameTab2\n/click PVPQueueFrameCategoryButton2")
-                else
-                    self:SetAttribute("macrotext", "/click ConquestJoinButton")
-                end
-            ]])
+--                 if PVEFrame:IsVisible() then
+--                     self:SetAttribute("macrotext", "/click LFDMicroButton")
+--                 elseif IsAltKeyDown() then
+--                     self:SetAttribute("macrotext", "/click LFDMicroButton\n/click PVEFrameTab2\n/click PVPQueueFrameCategoryButton1")
+--                 elseif GroupSizeButton ~= SelectedButton or IsControlKeyDown() then
+--                     self:SetAttribute("macrotext", "/click LFDMicroButton\n/click PVEFrameTab2\n/click PVPQueueFrameCategoryButton2")
+--                 else
+--                     self:SetAttribute("macrotext", "/click ConquestJoinButton")
+--                 end
+--             ]])
 
-            configureMacroButton = nil
-        end
+--             configureMacroButton = nil
+--         end
 
-        if not InCombat() then
-            configureMacroButton(joinMacroButton)
-        end
-    elseif eventName == "GROUP_ROSTER_UPDATE" then
-        joinMacroButton:SetFrameRef("GroupSizeButton", GetGroupSizeButton())
-    elseif eventName == "PLAYER_REGEN_DISABLED" then
-        joinMacroButton:Inactive("grayout")
-    elseif eventName == "PLAYER_REGEN_ENABLED" then
-        if configureMacroButton then
-            configureMacroButton(joinMacroButton)
-        end
-        joinMacroButton:Active("normal")
-    elseif eventName == "MODIFIER_STATE_CHANGED" then
-        local key, down = ...
-        if down == 1 and (key == "LALT" or key == "RALT") then
-            joinMacroButton:SetTexture("achievement_bg_winwsg")
-        else
-            joinMacroButton:SetTexture("achievement_bg_killxenemies_generalsroom")
-        end
-    end
-end)
+--         if not InCombat() then
+--             configureMacroButton(joinMacroButton)
+--         end
+--     elseif eventName == "GROUP_ROSTER_UPDATE" then
+--         joinMacroButton:SetFrameRef("GroupSizeButton", GetGroupSizeButton())
+--     elseif eventName == "PLAYER_REGEN_DISABLED" then
+--         joinMacroButton:Inactive("grayout")
+--     elseif eventName == "PLAYER_REGEN_ENABLED" then
+--         if configureMacroButton then
+--             configureMacroButton(joinMacroButton)
+--         end
+--         joinMacroButton:Active("normal")
+--     elseif eventName == "MODIFIER_STATE_CHANGED" then
+--         local key, down = ...
+--         if down == 1 and (key == "LALT" or key == "RALT") then
+--             joinMacroButton:SetTexture("achievement_bg_winwsg")
+--         else
+--             joinMacroButton:SetTexture("achievement_bg_killxenemies_generalsroom")
+--         end
+--     end
+-- end)
