ffba6ded424996a6304fdf8db127ce6c831d2e77
Genju <derfloh205@gmail.com>
2024-04-09 13:50:58 +0200

    tooltip from pvp data

--- Modules/PlayerTooltip/PlayerTooltip.lua
+++ Modules/PlayerTooltip/PlayerTooltip.lua
@@ -8,6 +8,10 @@ local debug = PvPAssistant.DEBUG:GetDebugPrint()
 ---@class PvPAssistant.PLAYER_TOOLTIP : Frame
 PvPAssistant.PLAYER_TOOLTIP = {}
 
+---@class PvPAssistant.PLAYER_TOOLTIP.BracketData
+---@field ratings table<PvPAssistant.Const.PVPModes, number>
+---@field shuffleSpecRatings table<number, number> specID -> rating
+
 function PvPAssistant.PLAYER_TOOLTIP:Init()
     TooltipDataProcessor.AddTooltipPostCall(Enum.TooltipDataType.Unit, function(_, data)
         local tooltipEnabled = PvPAssistant.DB.TOOLTIP_OPTIONS.PLAYER_TOOLTIP:IsEnabled()
@@ -24,7 +28,7 @@ function PvPAssistant.PLAYER_TOOLTIP:Init()
 end
 
 ---@param unit UnitId
----@return table?
+---@return PvPAssistant.PLAYER_TOOLTIP.BracketData?
 function PvPAssistant.PLAYER_TOOLTIP:GetPlayerPVPData(unit)
     if not unit then return nil end
 
@@ -34,8 +38,6 @@ function PvPAssistant.PLAYER_TOOLTIP:GetPlayerPVPData(unit)
 
     local pvpDataKey = unitName .. unitRealm
 
-    print("Fetching data for: " .. tostring(pvpDataKey))
-
     ---@diagnostic disable-next-line: undefined-field
     local unitPvPData = PvPAssistant.PVP_DATA[pvpDataKey]
 
@@ -43,23 +45,32 @@ function PvPAssistant.PLAYER_TOOLTIP:GetPlayerPVPData(unit)
         return
     end
 
-    local bracketPvPData = {}
+    ---@type PvPAssistant.PLAYER_TOOLTIP.BracketData
+    local bracketPvPData = {
+        ratings = {},
+        shuffleSpecRatings = {},
+    }
 
+    --- 2v2,3v3,rbg,shuffle-1,shuffle-2,shuffle-3,shuffle-4
     for index, mode in ipairs(PvPAssistant.CONST.PVP_DATA_BRACKET_ORDER) do
         local rating = unitPvPData[index]
-        bracketPvPData[mode] = rating
+        if index < 4 then
+            bracketPvPData.ratings[mode] = rating
+        else
+            local unitClassID = select(3, UnitClass(unit))
+            local specIndex = (3 - index) * -1
+            local specID = GetSpecializationInfoForClassID(unitClassID, specIndex)
+            if specID then
+                bracketPvPData.shuffleSpecRatings[specID] = rating
+            end
+        end
     end
-
-    DevTool:AddData(bracketPvPData, "datatest")
-
-    -- cache
-    --PvPAssistant.DB.PLAYER_DATA:Save(PvPAssistant.UTIL:GetPlayerUIDByUnit(unit), bracketPvPData)
 
     return bracketPvPData
 end
 
 ---@param unit UnitId
----@param bracketPvPData? table<PvPAssistant.Const.PVPModes, number>
+---@param bracketPvPData PvPAssistant.PLAYER_TOOLTIP.BracketData
 function PvPAssistant.PLAYER_TOOLTIP:UpdatePlayerTooltipByPvPData(unit, bracketPvPData)
     if not unit then return end
 
@@ -67,14 +78,22 @@ function PvPAssistant.PLAYER_TOOLTIP:UpdatePlayerTooltipByPvPData(unit, bracketP
 
     GameTooltip:AddLine(f.l(headerTitle))
 
-    for mode, rating in GUTIL:OrderedPairs(bracketPvPData, function(a, b) return a > b end) do
-        local isEnabled = false
-        if string.find(mode, PvPAssistant.CONST.PVP_MODES.SOLO_SHUFFLE) then
-            isEnabled = PvPAssistant.DB.TOOLTIP_OPTIONS.PLAYER_TOOLTIP:Get(PvPAssistant.CONST.PVP_MODES.SOLO_SHUFFLE)
-        end
+    for mode, rating in GUTIL:OrderedPairs(bracketPvPData.ratings, function(a, b) return a < b end) do
+        local isEnabled = PvPAssistant.DB.TOOLTIP_OPTIONS.PLAYER_TOOLTIP:Get(mode)
 
         if isEnabled then
             GameTooltip:AddDoubleLine(f.white(tostring(PvPAssistant.CONST.PVP_MODES_NAMES[mode])),
+                PvPAssistant.UTIL:ColorByRating(tostring(rating), rating))
+        end
+    end
+
+    if PvPAssistant.DB.TOOLTIP_OPTIONS.PLAYER_TOOLTIP:Get(PvPAssistant.CONST.PVP_MODES.SOLO_SHUFFLE) then
+        for specID, rating in pairs(bracketPvPData.shuffleSpecRatings) do
+            local icon = select(4, GetSpecializationInfoByID(specID))
+            local iconText = GUTIL:IconToText(icon, 15, 15)
+            GameTooltip:AddDoubleLine(
+                f.white(PvPAssistant.CONST.PVP_MODES_NAMES[PvPAssistant.CONST.PVP_MODES.SOLO_SHUFFLE]) .. " " ..
+                iconText,
                 PvPAssistant.UTIL:ColorByRating(tostring(rating), rating))
         end
     end
