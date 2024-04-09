9e193df6c0b57ef120f68ba866a883c7411d437b
Eyal Alon <3756641+iam3yal@users.noreply.github.com>
2024-04-09 18:42:35 +0300

    Update ArenaQuickJoin so it wouldn't load the PvP UI automatically
    Calling Active for the first time shows the button but it might remain grayed out and unclickable until the playe manually opens the PvP UI for the first time. Additionally, a tooltip is displayed indicating the player should do so.

--- Modules/ArenaQuickJoin/ArenaQuickJoin.lua
+++ Modules/ArenaQuickJoin/ArenaQuickJoin.lua
@@ -9,7 +9,6 @@ local PVPUI_ADDON_NAME = "Blizzard_PVPUI"
 local GameTooltip = GameTooltip
 local NewTicker = C_Timer.NewTicker
 local IsAddOnLoaded = C_AddOns.IsAddOnLoaded
-local UIParentLoadAddOn = UIParentLoadAddOn
 local InCombatLockdown = InCombatLockdown
 local UnitAffectingCombat = UnitAffectingCombat
 local IsControlKeyDown = IsControlKeyDown
@@ -44,67 +43,87 @@ local function GetSelectedBracketName(selectedBracketButton)
     end
 end
 
-local function ShowTooltipStateInfo(selectedBracketButton)
+local function ShowTooltipStateInfo(self, selectedBracketButton)
     GameTooltip:ClearLines()
 
-    local isFrameVisible = PVEFrame:IsVisible()
-    local groupSizeButton = GetGroupSizeButton()
-
-    if (IsControlKeyDown() or IsAltKeyDown()) and not isFrameVisible then
-        if IsControlKeyDown() then
-            GameTooltip:AddLine("Open PvP Rated tab.")
-        elseif IsAltKeyDown() then
-            GameTooltip:AddLine("Open the PvP Quick Match tab.")
-        end
-    elseif isFrameVisible then
-        GameTooltip:AddLine(("Close the %s frame."):format(DUNGEONS_BUTTON))
-    elseif groupSizeButton ~= selectedBracketButton then
-        if ConquestJoinButton:IsEnabled() then
-            GameTooltip:AddLine(RED_FONT_COLOR:WrapTextInColorCode(
-                "Click to open the PvP Rated tab, \nto select a bracket that matches your group size."))
-        else
-            GameTooltip:AddLine(RED_FONT_COLOR:WrapTextInColorCode(("Cannot join the selected bracket. The %s button is disabled.")
-                :format(BATTLEFIELD_JOIN)))
-        end
+    if not self:IsActivated() then
+        GameTooltip:AddLine("Activate the quick join button by opening the PvP UI once.")
     else
-        local bracketName = GetSelectedBracketName(selectedBracketButton)
-        if bracketName then
-            GameTooltip:AddLine(GREEN_FONT_COLOR:WrapTextInColorCode(("Click to queue to %s."):format(BLUE_FONT_COLOR
-                :WrapTextInColorCode(bracketName))))
+        local isFrameVisible = PVEFrame:IsVisible()
+        local groupSizeButton = GetGroupSizeButton()
+    
+        if (IsControlKeyDown() or IsAltKeyDown()) and not isFrameVisible then
+            if IsControlKeyDown() then
+                GameTooltip:AddLine("Open PvP Rated tab.")
+            elseif IsAltKeyDown() then
+                GameTooltip:AddLine("Open the PvP Quick Match tab.")
+            end
+        elseif isFrameVisible then
+            GameTooltip:AddLine(("Close the %s frame."):format(DUNGEONS_BUTTON))
+        elseif groupSizeButton ~= selectedBracketButton then
+            if ConquestJoinButton:IsEnabled() then
+                GameTooltip:AddLine(RED_FONT_COLOR:WrapTextInColorCode(
+                    "Click to open the PvP Rated tab, \nto select a bracket that matches your group size."))
+            else
+                GameTooltip:AddLine(RED_FONT_COLOR:WrapTextInColorCode(("Cannot join the selected bracket. The %s button is disabled.")
+                    :format(BATTLEFIELD_JOIN)))
+            end
+        else
+            local bracketName = GetSelectedBracketName(selectedBracketButton)
+            if bracketName then
+                GameTooltip:AddLine(GREEN_FONT_COLOR:WrapTextInColorCode(("Click to queue to %s."):format(BLUE_FONT_COLOR
+                    :WrapTextInColorCode(bracketName))))
+            end
         end
     end
 
     GameTooltip:Show()
 end
 
-local joinMacroButton, configureMacroButton, selectedBracketButton
+local joinMacroButton, configureMacroButton, selectedBracketButton, eventsRegistered
 frame:SetScript("OnEvent", function(_, eventName, ...)
     if eventName == "PLAYER_LOGIN" then
         do
             local parent = PvPAssistant.MAIN_FRAME:GetParentFrame()
-
+            
             joinMacroButton = CreateFrame("Button", "PvPAssistant_ArenaQuickJoinMacroButton", parent,
                 "SecureActionButtonTemplate, SecureHandlerStateTemplate, ActionButtonTemplate")
+
+            local function RegisterEvents()
+                frame:RegisterEvent("ADDON_LOADED")
+                frame:RegisterEvent("PLAYER_REGEN_DISABLED")
+                frame:RegisterEvent("PLAYER_REGEN_ENABLED")
+                frame:RegisterEvent("GROUP_ROSTER_UPDATE")
+                frame:RegisterEvent("MODIFIER_STATE_CHANGED")
+                eventsRegistered = true
+            end
+        
+            local function UnregisterEvents()
+                frame:UnregisterEvent("ADDON_LOADED")
+                frame:UnregisterEvent("PLAYER_REGEN_DISABLED")
+                frame:UnregisterEvent("PLAYER_REGEN_ENABLED")
+                frame:UnregisterEvent("GROUP_ROSTER_UPDATE")
+                frame:UnregisterEvent("MODIFIER_STATE_CHANGED")
+                eventsRegistered = false
+            end
 
             function joinMacroButton:Active(style)
                 if style == "show" then
                     self:SetAlpha(1)
                 elseif style == "normal" then
-                    -- NOTE: Can't be called during combat
-                    self:Enable()
+                    -- NOTE: Can't be called during combat.
+                    self:RegisterForClicks('AnyUp', 'AnyDown')
                     self.icon:SetDesaturated(false)
                 else
-                    -- NOTE: Can't be called during combat
-                    frame:RegisterEvent("ADDON_LOADED")
-                    frame:RegisterEvent("PLAYER_REGEN_DISABLED")
-                    frame:RegisterEvent("PLAYER_REGEN_ENABLED")
-                    frame:RegisterEvent("GROUP_ROSTER_UPDATE")
-                    frame:RegisterEvent("MODIFIER_STATE_CHANGED")
-                    local _, isLoaded = IsAddOnLoaded(PVPUI_ADDON_NAME)
-                    if not isLoaded then
-                        UIParentLoadAddOn(PVPUI_ADDON_NAME)
-                    end
+                    -- NOTE: Can't be called during combat.
                     self:Show()
+                    local _, isLoaded = IsAddOnLoaded(PVPUI_ADDON_NAME)
+                    if isLoaded then
+                        self:Active("normal")
+                    end
+                    if not eventsRegistered then
+                        RegisterEvents()
+                    end
                 end
             end
 
@@ -112,23 +131,21 @@ frame:SetScript("OnEvent", function(_, eventName, ...)
                 if style == "hide" then
                     self:SetAlpha(0)
                 elseif style == "grayout" then
-                    -- NOTE: Can't be called during combat
-                    self:Disable()
+                    -- NOTE: Can't be called during combat.
+                    -- NOTE: We're using RegisterForClicks as opposed to Disable because when it's truly disabled OnEnter and OnLeave aren't fired.
+                    self:RegisterForClicks()
                     self.icon:SetDesaturated(true)
                 else
-                    -- NOTE: Can't be called during combat
+                    -- NOTE: Can't be called during combat.
                     self:Hide()
                     self:Inactive("grayout")
-                    frame:UnregisterEvent("ADDON_LOADED")
-                    frame:UnregisterEvent("PLAYER_REGEN_DISABLED")
-                    frame:UnregisterEvent("PLAYER_REGEN_ENABLED")
-                    frame:UnregisterEvent("GROUP_ROSTER_UPDATE")
-                    frame:UnregisterEvent("MODIFIER_STATE_CHANGED")
+                    UnregisterEvents()
                 end
             end
 
             function joinMacroButton:CanBeActivated()
-                return not InCombat()
+                local _, isLoaded = IsAddOnLoaded(PVPUI_ADDON_NAME)
+                return isLoaded and not InCombat()
             end
 
             function joinMacroButton:IsActivated()
@@ -143,7 +160,6 @@ frame:SetScript("OnEvent", function(_, eventName, ...)
             joinMacroButton:SetPoint("TOP", parent, "TOP", 275, -35)
             joinMacroButton:SetSize(45, 45)
             joinMacroButton:SetScale(.6)
-            joinMacroButton:RegisterForClicks('AnyUp', 'AnyDown')
             joinMacroButton:SetTexture("achievement_bg_killxenemies_generalsroom")
             joinMacroButton:SetAttribute("type", "macro")
             joinMacroButton:Inactive()
@@ -153,7 +169,7 @@ frame:SetScript("OnEvent", function(_, eventName, ...)
             local tooltipHandle
 
             local tooltip = function()
-                ShowTooltipStateInfo(selectedBracketButton)
+                ShowTooltipStateInfo(joinMacroButton, selectedBracketButton)
             end
 
             local showAndUpdateTooltip = function()
@@ -182,8 +198,8 @@ frame:SetScript("OnEvent", function(_, eventName, ...)
         end
 
         configureMacroButton = function(self)
-            self:Active("normal")
-
+            self:Active()
+            
             self:SetFrameRef("PVEFrame", PVEFrame)
             self:SetFrameRef("GroupSizeButton", GetGroupSizeButton())
             self:SetFrameRef("ConquestJoinButton", ConquestJoinButton)
@@ -218,27 +234,30 @@ frame:SetScript("OnEvent", function(_, eventName, ...)
                 end
             ]])
 
+            frame:UnregisterEvent("ADDON_LOADED")
             configureMacroButton = nil
         end
 
         if not InCombat() then
             configureMacroButton(joinMacroButton)
         end
-    elseif eventName == "GROUP_ROSTER_UPDATE" then
-        joinMacroButton:SetFrameRef("GroupSizeButton", GetGroupSizeButton())
-    elseif eventName == "PLAYER_REGEN_DISABLED" then
-        joinMacroButton:Inactive("grayout")
-    elseif eventName == "PLAYER_REGEN_ENABLED" then
-        if configureMacroButton then
-            configureMacroButton(joinMacroButton)
-        end
-        joinMacroButton:Active("normal")
-    elseif eventName == "MODIFIER_STATE_CHANGED" then
-        local key, down = ...
-        if down == 1 and (key == "LALT" or key == "RALT") then
-            joinMacroButton:SetTexture("achievement_bg_winwsg")
-        else
-            joinMacroButton:SetTexture("achievement_bg_killxenemies_generalsroom")
+    elseif joinMacroButton:IsActivated() then
+        if eventName == "GROUP_ROSTER_UPDATE" then
+            joinMacroButton:SetFrameRef("GroupSizeButton", GetGroupSizeButton())
+        elseif eventName == "PLAYER_REGEN_DISABLED" then
+            joinMacroButton:Inactive("grayout")
+        elseif eventName == "PLAYER_REGEN_ENABLED" then
+            if configureMacroButton then
+                configureMacroButton(joinMacroButton)
+            end
+            joinMacroButton:Active("normal")
+        elseif eventName == "MODIFIER_STATE_CHANGED" then
+            local key, down = ...
+            if down == 1 and (key == "LALT" or key == "RALT") then
+                joinMacroButton:SetTexture("achievement_bg_winwsg")
+            else
+                joinMacroButton:SetTexture("achievement_bg_killxenemies_generalsroom")
+            end
         end
     end
 end)
