604fcbb4721d4f380a2a0bc22978b99b0e4ec583
Genju <derfloh205@gmail.com>
2024-04-18 09:16:19 +0200

    db

---
+++ DB/RecommendationDB.lua
@@ -0,0 +1,44 @@
+---@class PvPAssistant
+local PvPAssistant = select(2, ...)
+
+---@class PvPAssistant.DB
+PvPAssistant.DB = PvPAssistant.DB
+
+---@class PvPAssistant.DB.RECOMMENDATION_DATA
+PvPAssistant.DB.RECOMMENDATION_DATA = {}
+
+---@alias UnitGUID string
+---@class PvPAssistant.RecommendationData
+---@field note string
+---@field rating number
+
+function PvPAssistant.DB.RECOMMENDATION_DATA:Init()
+    if not PvPAssistantDB.recommendationData then
+        PvPAssistantDB.recommendationData = {
+            version = 1,
+            ---@type table<UnitGUID, PvPAssistant.RecommendationData>
+            data = {}
+        }
+    end
+end
+
+function PvPAssistant.DB.RECOMMENDATION_DATA:Clear()
+    wipe(PvPAssistantDB.recommendationData.data)
+end
+
+---@return PvPAssistant.RecommendationData? unitData
+function PvPAssistant.DB.RECOMMENDATION_DATA:Get(unitGUID)
+    return PvPAssistantDB.recommendationData.data[unitGUID]
+end
+
+---@param unitData PvPAssistant.RecommendationData
+function PvPAssistant.DB.RECOMMENDATION_DATA:Save(unitGUID, unitData)
+    PvPAssistantDB.recommendationData.data[unitGUID] = unitData
+end
+
+---@return PvPAssistant.RecommendationData[]
+function PvPAssistant.DB.RECOMMENDATION_DATA:GetAll()
+    return PvPAssistantDB.recommendationData.data
+end
+
+function PvPAssistant.DB.RECOMMENDATION_DATA:Migrate() end
