0ea76eb7dbf079fe826b54a9ee3e93e99e25bd7d
Genju <derfloh205@gmail.com>
2024-04-24 10:35:19 +0200

    test data adaptions

--- PvPData/_PvPData.lua
+++ PvPData/_PvPData.lua
@@ -2,5 +2,7 @@
 local PvPAssistant = select(2, ...)
 
 PvPAssistant.PVP_DATA = {
-	["Slarky-thrall"] = { 1916, 2245, 0, 2250, 0, 543 },
+	["thrall"] = {
+		["Slarky"] = { 1916, 2245, 0, 2250, 0, 543 },
+	},
 }
