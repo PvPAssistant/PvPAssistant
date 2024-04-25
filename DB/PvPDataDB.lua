19e97ad16f36c6c5d07cd3628d01b3e0b0593a9c
Florian Schneider <derfloh205@gmail.com>
2024-04-25 15:25:12 +0200

    Feature/#151 pvp data structure (#153)
    * remove data, adapt test data
    
    * test data adaptions
    
    * test data
    
    * added cbor serialized test data
    
    * adaption to cbor serialized data injection
    
    * finalized!
    
    * small fixes

--- DB/PvPDataDB.lua
+++ DB/PvPDataDB.lua
@@ -11,26 +11,42 @@ PvPAssistant.DB.PVP_DATA = {}
 ---@field ratings table<PvPAssistant.Const.PVPModes, number>
 ---@field shuffleSpecRatings table<number, number> specID -> rating
 
+function PvPAssistant.DB.PVP_DATA:Init()
+    PvPAssistant.DB.DEBUG:ClearAll()
+
+    local libCBOR = LibStub("LibCBOR-1.0")
+
+    local cborString = PvPAssistant.PVP_DATA
+    local deserializedData = libCBOR:Deserialize(cborString)
+
+    PvPAssistant.PVP_DATA = deserializedData
+end
+
 ---@return PlayerPvPData? bracketData
 function PvPAssistant.DB.PVP_DATA:GetByUnit(unit)
     if not unit then return nil end
+
 
     local unitName, unitRealm = UnitNameUnmodified(unit)
     unitRealm = unitRealm or GetNormalizedRealmName()
     unitRealm = PvPAssistant.UTIL:CamelCaseToDashSeparated(unitRealm) -- temporary adaption to pvp data format
 
-    local playerUIDDashSeparated = unitName .. unitRealm
+    local data = self:Get(unitName, unitRealm, select(2, UnitClass(unit)))
 
-    return self:Get(playerUIDDashSeparated)
+    return data
 end
 
----@param playerUIDDashSeparated string -- e.g. slarky-tarren-mill
+---@param characterName string -- e.g. Slarky
+---@param realmName string -- e.g. tarren-mill
 ---@param class? ClassFile
-function PvPAssistant.DB.PVP_DATA:Get(playerUIDDashSeparated, class)
-    ---@diagnostic disable-next-line: undefined-field
-    local unitPvPData = PvPAssistant.PVP_DATA[playerUIDDashSeparated]
+function PvPAssistant.DB.PVP_DATA:Get(characterName, realmName, class)
+    local realmPlayers = PvPAssistant.PVP_DATA[realmName]
+    if not realmPlayers then return end
 
-    if not unitPvPData then
+    ---@diagnostic disable-next-line: undefined-field
+    local dbData = realmPlayers[characterName]
+
+    if not dbData then
         return
     end
 
@@ -42,11 +58,11 @@ function PvPAssistant.DB.PVP_DATA:Get(playerUIDDashSeparated, class)
 
     --- 2v2,3v3,rbg,shuffle-1,shuffle-2,shuffle-3,shuffle-4
     for index, mode in ipairs(PvPAssistant.CONST.PVP_DATA_BRACKET_ORDER) do
-        local rating = unitPvPData[index]
+        local rating = dbData[index]
         if index < 4 then
             playerPvPData.ratings[mode] = rating
         elseif class then
-            local unitClassID = select(3, class)
+            local unitClassID = PvPAssistant.CONST.CLASS_ID[class]
             local specIndex = (3 - index) * -1
             local specID = GetSpecializationInfoForClassID(unitClassID, specIndex)
             if specID then
